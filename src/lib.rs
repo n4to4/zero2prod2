e3afc74029f961a9331b1b35c81356bd0baf5718
n4to4 <n4to4k@gmail.com>
2022-08-19 06:18:54 +0900

    subscriptions

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,12 @@
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {name}!")
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
-async fn health_check() -> HttpResponse {
+async fn subscriptions() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
@@ -15,8 +14,7 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
+            .route("/subscriptions", web::post().to(subscriptions))
     })
     .listen(listener)?
     .run();
