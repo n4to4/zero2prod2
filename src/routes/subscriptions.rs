992fd1a872ef9de0d620171844670cda24e5c530
n4to4 <n4to4k@gmail.com>
2022-08-25 08:57:19 +0900

    instrument

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -12,16 +12,12 @@ pub struct FormData {
     email: String,
 }
 
+#[tracing::instrument(name = "Adding a new subscriber", skip(form, pool), fields(
+    request_id = %Uuid::new_v4(),
+    subscriber_email = %form.email,
+    subscriber_name = %form.name,
+))]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    let request_id = Uuid::new_v4();
-    let request_span = tracing::info_span!(
-        "Adding a new subscriber.",
-        %request_id,
-        subscriber_email=%form.email,
-        subscriber_name=%form.name
-    );
-    let _request_span_guard = request_span.enter();
-
     let query_span = tracing::info_span!("Saving new subscriber details in the database");
     match sqlx::query!(
         r#"
@@ -39,11 +35,7 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(e) => {
-            tracing::error!(
-                "request_id {} - Failed to execute query: {:?}",
-                request_id,
-                e
-            );
+            tracing::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
