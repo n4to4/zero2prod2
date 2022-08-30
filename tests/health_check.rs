a9e82fd17ee87db490c15c6e3da23a4629d29dc3
n4to4 <n4to4k@gmail.com>
2022-08-30 18:55:01 +0900

    wip

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -88,12 +88,6 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     let app = spawn_app().await;
     let client = reqwest::Client::new();
 
-    //let configuration = get_configuration().expect("Failed to read configuration");
-    //let connection_string = configuration.database.connection_string();
-    //let mut connection = PgConnection::connect(&connection_string)
-    //    .await
-    //    .expect("Failed to connect to Postgres.");
-
     // Act
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
     let response = client
