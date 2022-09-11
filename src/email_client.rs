0b10828fb41b5dfdcc528d585a49e166a9a3b8f0
n4to4 <n4to4k@gmail.com>
2022-09-11 09:11:23 +0900

    green

--- src/email_client.rs
+++ src/email_client.rs
@@ -46,7 +46,8 @@ impl EmailClient {
             )
             .json(&request_body)
             .send()
-            .await?;
+            .await?
+            .error_for_status()?;
         Ok(())
     }
 }
