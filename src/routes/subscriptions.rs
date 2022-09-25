c7e56e8172979de34b40d35b20bbea653dac1886
n4to4 <n4to4k@gmail.com>
2022-09-25 09:34:27 +0900

    error handling

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -15,7 +15,6 @@ pub struct FormData {
     email: String,
 }
 
-#[derive(Debug)]
 pub struct StoreTokenError(sqlx::Error);
 
 impl ResponseError for StoreTokenError {}
@@ -27,6 +26,18 @@ impl std::fmt::Display for StoreTokenError {
             "A database error was encountered while \
             trying to store a subscription token."
         )
+    }
+}
+
+impl std::fmt::Debug for StoreTokenError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}\nCaused by:\n\t{}", self, self.0)
+    }
+}
+
+impl std::error::Error for StoreTokenError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        Some(&self.0)
     }
 }
 
