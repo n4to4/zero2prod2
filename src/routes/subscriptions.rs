86e62d5be50cacb006c7ae99791da59f8deb51ac
n4to4 <n4to4k@gmail.com>
2022-09-28 18:43:42 +0900

    error handling

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -124,11 +124,7 @@ pub async fn insert_subscriber(
         Utc::now()
     )
     .execute(transaction)
-    .await
-    .map_err(|e| {
-        tracing::error!("Failed to execute query: {:?}", e);
-        e
-    })?;
+    .await?;
     Ok(subscriber_id)
 }
 
@@ -177,10 +173,7 @@ pub async fn store_token(
     )
     .execute(transaction)
     .await
-    .map_err(|e| {
-        tracing::error!("Failed to execute query: {:?}", e);
-        StoreTokenError(e)
-    })?;
+    .map_err(StoreTokenError)?;
     Ok(())
 }
 
