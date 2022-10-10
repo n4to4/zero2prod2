a31c4eff7c4ca85148e65ba28a01fc0188e3b014
n4to4 <n4to4k@gmail.com>
2022-10-10 09:22:48 +0900

    login-error

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -17,28 +17,28 @@ pub async fn login_form(query: web::Query<QueryParams>) -> HttpResponse {
             r#"<!DOCTYPE html>
 <html lang="en">
 <head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8">
-<title>Login</title>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8">
+  <title>Login</title>
 </head>
 <body>
-{error_html}
-<form action="/login" method="post">
-<label>Username
-<input
-type="text"
-placeholder="Enter Username"
-name="username"
->
-</label>
-<label>Password
-<input
-type="password"
-placeholder="Enter Password"
-name="password"
->
-</label>
-<button type="submit">Login</button>
-</form>
+  {error_html}
+  <form action="/login" method="post">
+    <label>Username
+      <input
+        type="text"
+        placeholder="Enter Username"
+        name="username"
+      >
+    </label>
+    <label>Password
+      <input
+        type="password"
+        placeholder="Enter Password"
+        name="password"
+      >
+    </label>
+    <button type="submit">Login</button>
+  </form>
 </body>
 </html>"#
         ))
