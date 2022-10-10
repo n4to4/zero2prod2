3aa30d713dbb0d9be0e17c662038974e48bfce5e
n4to4 <n4to4k@gmail.com>
2022-10-10 09:16:30 +0900

    login-error

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,7 +1,13 @@
 use actix_web::http::header::ContentType;
-use actix_web::HttpResponse;
+use actix_web::{web, HttpResponse};
 
-pub async fn login_form() -> HttpResponse {
+#[derive(serde::Deserialize)]
+pub struct QueryParams {
+    error: Option<String>,
+}
+
+pub async fn login_form(query: web::Query<QueryParams>) -> HttpResponse {
+    let _error = query.0.error;
     HttpResponse::Ok()
         .content_type(ContentType::html())
         .body(include_str!("login.html"))
