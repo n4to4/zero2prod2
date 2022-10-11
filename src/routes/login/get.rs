8cbe42eb38c9da7a3932908e0f1ef7a1fa5e44f5
n4to4 <n4to4k@gmail.com>
2022-10-11 17:01:56 +0900

    login

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,17 +1,24 @@
+use crate::startup::HmacSecret;
 use actix_web::http::header::ContentType;
 use actix_web::{web, HttpResponse};
+use hmac::{Hmac, Mac};
+use secrecy::ExposeSecret;
 
 #[derive(serde::Deserialize)]
 pub struct QueryParams {
-    error: Option<String>,
+    error: String,
+    tag: String,
 }
 
-pub async fn login_form(query: web::Query<QueryParams>) -> HttpResponse {
-    let error_html = match query.0.error {
+pub async fn login_form(
+    query: Option<web::Query<QueryParams>>,
+    secret: web::Data<HmacSecret>,
+) -> HttpResponse {
+    let error_html = match query {
         None => String::new(),
-        Some(error_message) => format!(
+        Some(query) => format!(
             "<p><i>{}</i></p>",
-            htmlescape::encode_minimal(&error_message)
+            htmlescape::encode_minimal(&query.0.error)
         ),
     };
     HttpResponse::Ok()
@@ -45,4 +52,18 @@ pub async fn login_form(query: web::Query<QueryParams>) -> HttpResponse {
 </body>
 </html>"#
         ))
+}
+
+impl QueryParams {
+    fn verify(self, secret: &HmacSecret) -> Result<String, anyhow::Error> {
+        let tag = hex::decode(self.tag)?;
+        let query_string = format!("error={}", urlencoding::Encoded::new(&self.error));
+
+        let mut mac =
+            Hmac::<sha2::Sha256>::new_from_slice(secret.0.expose_secret().as_bytes()).unwrap();
+        mac.update(query_string.as_bytes());
+        mac.verify_slice(&tag)?;
+
+        Ok(self.error)
+    }
 }
