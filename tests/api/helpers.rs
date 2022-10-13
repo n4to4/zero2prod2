b796dbab9691e74f8899c8b7014f06fa536a1ea1
n4to4 <n4to4k@gmail.com>
2022-10-13 17:13:43 +0900

    login

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -190,3 +190,8 @@ impl TestUser {
         .expect("Failed to store test user.");
     }
 }
+
+pub fn assert_is_redirect_to(response: &reqwest::Response, location: &str) {
+    assert_eq!(response.status().as_u16(), 303);
+    assert_eq!(response.headers().get("Location").unwrap(), location);
+}
