c66c80080927909e6ed39023c1e769ff16570506
n4to4 <n4to4k@gmail.com>
2022-10-26 17:42:12 +0900

    green

--- tests/api/admin_dashboard.rs
+++ tests/api/admin_dashboard.rs
@@ -35,7 +35,7 @@ async fn logout_clears_session_state() {
 
     // Act - Part 4 - Follow the redirect
     let html_page = app.get_login_html().await;
-    assert!(html_page.contains(r#"<p><i>You have successfully logged out.</i><p>"#));
+    assert!(html_page.contains(r#"<p><i>You have successfully logged out.</i></p>"#));
 
     // Act - Part 5 - Attempt to load admin panel
     let response = app.get_admin_dashboard().await;
