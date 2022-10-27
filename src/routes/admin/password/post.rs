aa8e0adb89df4608a21f8ff9b2929c1bf0626407
n4to4 <n4to4k@gmail.com>
2022-10-27 17:59:47 +0900

    refactor

--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -2,10 +2,12 @@ use crate::authentication::{validate_credentials, AuthError, Credentials};
 use crate::routes::admin::dashboard::get_username;
 use crate::session_state::TypedSession;
 use crate::utils::{e500, see_other};
+use actix_web::error::InternalError;
 use actix_web::{web, HttpResponse};
 use actix_web_flash_messages::FlashMessage;
 use secrecy::{ExposeSecret, Secret};
 use sqlx::PgPool;
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -19,12 +21,7 @@ pub async fn change_password(
     session: TypedSession,
     pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, actix_web::Error> {
-    let user_id = session.get_user_id().map_err(e500)?;
-    let user_id = match user_id {
-        Some(id) => id,
-        None => return Ok(see_other("/login")),
-    };
-
+    let user_id = reject_anonymous_users(session).await?;
     if form.new_password.expose_secret() != form.new_password_check.expose_secret() {
         FlashMessage::error(
             "You entered two different new passwords - the field values must match.",
@@ -51,4 +48,15 @@ pub async fn change_password(
         .map_err(e500)?;
     FlashMessage::error("Your password has been changed.").send();
     Ok(see_other("/admin/password"))
+}
+
+async fn reject_anonymous_users(session: TypedSession) -> Result<Uuid, actix_web::Error> {
+    match session.get_user_id().map_err(e500)? {
+        Some(user_id) => Ok(user_id),
+        None => {
+            let response = see_other("/login");
+            let e = anyhow::anyhow!("The user has not logged in");
+            Err(InternalError::from_response(e, response).into())
+        }
+    }
 }
