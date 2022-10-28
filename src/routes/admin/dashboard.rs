b8045e52d643765292be3dcf67c2bd19e4f1c0b9
n4to4 <n4to4k@gmail.com>
2022-10-28 17:41:07 +0900

    Remove login-checks for /admin/* pages

--- src/routes/admin/dashboard.rs
+++ src/routes/admin/dashboard.rs
@@ -1,5 +1,5 @@
-use crate::session_state::TypedSession;
-use crate::utils::{e500, see_other};
+use crate::authentication::UserId;
+use crate::utils::e500;
 use actix_web::http::header::ContentType;
 use actix_web::{web, HttpResponse};
 use anyhow::Context;
@@ -7,14 +7,11 @@ use sqlx::PgPool;
 use uuid::Uuid;
 
 pub async fn admin_dashboard(
-    session: TypedSession,
     pool: web::Data<PgPool>,
+    user_id: web::ReqData<UserId>,
 ) -> Result<HttpResponse, actix_web::Error> {
-    let username = if let Some(user_id) = session.get_user_id().map_err(e500)? {
-        get_username(user_id, &pool).await.map_err(e500)?
-    } else {
-        return Ok(see_other("/login"));
-    };
+    let user_id = user_id.into_inner();
+    let username = get_username(*user_id, &pool).await.map_err(e500)?;
     Ok(HttpResponse::Ok()
         .content_type(ContentType::html())
         .body(format!(
