fb5da148b6bee6b005e32f88d03a422e72cafb29
n4to4 <n4to4k@gmail.com>
2022-11-04 09:20:28 +0900

    no idempotency

--- src/routes/admin/newsletter/get.rs
+++ src/routes/admin/newsletter/get.rs
@@ -1,44 +1,52 @@
 use actix_web::http::header::ContentType;
 use actix_web::HttpResponse;
+use actix_web_flash_messages::IncomingFlashMessages;
+use std::fmt::Write;
 
-pub async fn newsletter_form() -> HttpResponse {
-    println!("GET newsletter form");
-    HttpResponse::Ok().content_type(ContentType::html()).body(
-        r#"<!DOCTYPE html>
+pub async fn newsletter_form(flash_messages: IncomingFlashMessages) -> HttpResponse {
+    let mut msg_html = String::new();
+    for m in flash_messages.iter() {
+        writeln!(msg_html, "<p><i>{}</i></p>", m.content()).unwrap();
+    }
+    HttpResponse::Ok()
+        .content_type(ContentType::html())
+        .body(format!(
+            r#"<!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>Submit New Issue</title>
 </head>
-<meta http-equiv="content-type" content="text/html; charset=utf-8">
-<title>Submit New Issue</title>
 <body>
-<form action="/admin/newsletters" method="post">
-<label>Title
-    <input
-        type="text"
-        placeholder="Enter issue title"
-        name="title"
-    >
-</label>
-<br>
-<label>HTML Content
-    <input
-        type="text"
-        placeholder="Enter HTML content"
-        name="content_html"
-    >
-</label>
-<br>
-<label>Text Content
-    <input
-        type="text"
-        placeholder="Enter text content"
-        name="content_text"
-    >
-</label>
-<button type="submit">Submit new issue</button>
-</form>
-<p><a href="/admin/dashboard">&gt;- Back</a></p>
+    {msg_html}
+    <form action="/admin/newsletters" method="post">
+        <label>Title
+            <input
+                type="text"
+                placeholder="Enter issue title"
+                name="title"
+            >
+        </label>
+        <br>
+        <label>HTML Content
+            <input
+                type="text"
+                placeholder="Enter HTML content"
+                name="content_html"
+            >
+        </label>
+        <br>
+        <label>Text Content
+            <input
+                type="text"
+                placeholder="Enter text content"
+                name="content_text"
+            >
+        </label>
+        <button type="submit">Submit new issue</button>
+    </form>
+    <p><a href="/admin/dashboard">&gt;- Back</a></p>
 </body>
 </html>"#,
-    )
+        ))
 }
--- tests/api/newsletter.rs
+++ tests/api/newsletter.rs
@@ -232,3 +232,45 @@ async fn can_get_admin_newsletters() {
     // Assert
     assert!(html_page.contains("Submit new issue"));
 }
+
+// ---
+
+#[tokio::test]
+async fn newsletter_creation_is_idempotent() {
+    // Arrange
+    let app = spawn_app().await;
+    create_confirmed_subscriber(&app).await;
+    //app.test_user.login(&app).await;
+    app.login().await;
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .expect(1)
+        .mount(&app.email_server)
+        .await;
+
+    // Act - Part 1 - Submit newsletter form
+    let newsletter_request_body = serde_json::json!({
+        "title": "Newsletter title",
+        "text_content": "Newsletter body as plain text",
+        "html_content": "<p>Newsletter body as HTML</p>",
+        "idempotency_key": uuid::Uuid::new_v4().to_string()
+    });
+    let response = app.post_newsletters_admin(&newsletter_request_body).await;
+    assert_is_redirect_to(&response, "/admin/newsletters");
+
+    // Act - Part 2 - Follow the redirect
+    let html_page = app.get_admin_newsletters_html().await;
+    assert!(html_page.contains("<p><i>The newsletter issue has been published!</i></p>"));
+
+    // Act - Part 3 - Submit newsletter form **again**
+    let response = app.post_newsletters_admin(&newsletter_request_body).await;
+    assert_is_redirect_to(&response, "/admin/newsletters");
+
+    // Act - Part 4 - Follow the redirect
+    let html_page = app.get_admin_newsletters_html().await;
+    assert!(html_page.contains("<p><i>The newsletter issue has been published!</i></p>"));
+
+    // Mock verifies on Drop that we have sent the newsletter email **once**
+}
