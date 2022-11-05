8246f37f4ebbdda99116fa507f5e40744cc0ecb5
n4to4 <n4to4k@gmail.com>
2022-11-05 10:34:33 +0900

    compiles

--- src/idempotency/persistence.rs
+++ src/idempotency/persistence.rs
@@ -1,4 +1,5 @@
 use super::IdempotencyKey;
+use actix_web::body::to_bytes;
 use actix_web::http::StatusCode;
 use actix_web::HttpResponse;
 use sqlx::PgPool;
@@ -42,4 +43,29 @@ pub async fn get_saved_response(
     } else {
         Ok(None)
     }
+}
+
+pub async fn save_response(
+    pool: &PgPool,
+    idempotency_key: &IdempotencyKey,
+    user_id: Uuid,
+    http_response: HttpResponse,
+) -> anyhow::Result<HttpResponse> {
+    let (response_head, body) = http_response.into_parts();
+    let body = to_bytes(body).await.map_err(|e| anyhow::anyhow!("{}", e))?;
+    let status_code = response_head.status().as_u16() as i16;
+    let headers = {
+        let mut h = Vec::with_capacity(response_head.headers().len());
+        for (name, value) in response_head.headers().iter() {
+            let name = name.as_str().to_owned();
+            let value = value.as_bytes().to_owned();
+            h.push(HeaderPairRecord { name, value })
+        }
+        h
+    };
+
+    // TODO: SQL query
+
+    let http_response = response_head.set_body(body).map_into_boxed_body();
+    Ok(http_response)
 }
